db8341ffd257d773b68ad9d1110f18f4c0b9d4c3
mikaeleythor <eythor108@gmail.com>
2023-03-28 12:51:15 +0000

    I think its fixed

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -60,14 +60,8 @@ cmp.setup {
 				end
 			end
 		}),
-		['<C-Space>'] = cmp.mapping.complete(),
-
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-      -- they way you will only jump inside the snippet region
-      elseif luasnip.expand_or_jumpable() then
+		['<C-Space>'] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
@@ -76,15 +70,29 @@ cmp.setup {
       end
     end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    --["<Tab>"] = cmp.mapping(function(fallback)
+      --if cmp.visible() then
+        --cmp.select_next_item()
+      ---- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+      ---- they way you will only jump inside the snippet region
+      --elseif luasnip.expand_or_jumpable() then
+        --luasnip.expand_or_jump()
+      --elseif has_words_before() then
+        --cmp.complete()
+      --else
+        --fallback()
+      --end
+    --end, { "i", "s" }),
+
+    --["<S-Tab>"] = cmp.mapping(function(fallback)
+      --if cmp.visible() then
+        --cmp.select_prev_item()
+      --elseif luasnip.jumpable(-1) then
+        --luasnip.jump(-1)
+      --else
+        --fallback()
+      --end
+    --end, { "i", "s" }),
 	},
 
 	-- TODO: Configure max_item_count
