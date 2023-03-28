12a213a5d425d4669827b4fc1bbe7f6831a4dce9
mikaeleythor <eythor108@gmail.com>
2023-03-28 12:42:04 +0000

    laga mappings

--- ftplugin/sql.lua
+++ ftplugin/sql.lua
@@ -1,7 +1,13 @@
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.cmdheight = 4
+
 local wk = require("which-key")
-local opts = { noremap = true, silent = true, prefix = "<leader>", buffer = 0 }
+local opts = { noremap = true, silent = true, buffer = 0 }
 
 wk.register({
+	--["<leader>lf"] = { "<cmd>!sqlformat % --output %<cr>", "Format" },
+	--["<leader>ll"] = { "<cmd>!sqlfluff lint --dialect postgres %<cr>", "Lint" }
 	["<leader>lf"] = { "<cmd>!sqlfluff fix --dialect postgres --force %<cr>", "Format" },
 	["<leader>ll"] = { "<cmd>!sqlfluff lint --dialect postgres %<cr>", "Lint" }
 }, opts)
