18e90aabefdec2329c903c90d2379290aeb162eb
mikaeleythor <eythor108@gmail.com>
2023-07-17 13:56:43 +0000

    Switching telescope file browser out for oil.nvim

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,28 +1,9 @@
 local telescope = require("telescope")
-local fb_actions = telescope.extensions.file_browser.actions
 telescope.setup({
 	defaults = {
 		file_ignore_patterns = {
 			"node_modules"
 		}
 	},
-	extensions = {
-		file_browser = {
-			hijack_netrw = true,
-			hide_parent_dir = true,
-			mappings = {
-				["i"] = {
-					["<C-u>"] = fb_actions.goto_parent_dir,
-					["<C-a>"] = fb_actions.create,
-				},
-				["n"] = {
-					["u"] = fb_actions.goto_parent_dir,
-					["a"] = fb_actions.create,
-				}
-
-			}
-		},
-	},
 })
 telescope.load_extension('possession')
-telescope.load_extension('file_browser')
