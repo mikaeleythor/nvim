08a2c37751655a702fca1e0fd10df31652ca5505
mikaeleythor <eythor108@gmail.com>
2023-07-17 13:55:19 +0000

    documentation

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -2,59 +2,69 @@ vim.bo.autoindent = true
 
 vim.opt.termguicolors = true
 
--- Linewidth
+-- NOTE: Linewidth
 vim.wo.cc = "80"
 
--- Highlight current line
+-- NOTE: Highlight current line
 vim.wo.cursorline = true
 
--- Highlight search results
+-- NOTE: Highlight search results
 vim.go.hlsearch = true
 
--- Ignore case when searching
+-- NOTE: Ignore case when searching
 vim.go.ignorecase = true
 
--- Search incrementally (traverse between results with n/N)
+-- NOTE: Search incrementally (traverse between results with n/N)
 vim.go.incsearch = true
 
--- Split screen buffers
+-- NOTE: Split screen buffers
 vim.go.splitright = true
 vim.go.splitbelow = true
 
--- Count relative to current line
+-- NOTE: Count relative to current line
 vim.wo.relativenumber = true
 
--- Expand tabs to spaces
+-- NOTE: Expand tabs to spaces
 vim.bo.expandtab = true
 
--- This sets the clipboard to global
+-- NOTE: This sets the clipboard to global
 vim.go.clipboard = "unnamedplus"
 
--- Show number of current line
+-- WARNING: Testing this for image.nvim
+vim.opt.conceallevel = 2
+vim.opt.signcolumn = "yes:2"
+
+-- NOTE: Show number of current line
 vim.wo.number = true
 vim.g.syntax_on = true
 
--- Important for livepreview
+-- WARNING: Set for 'livepreview'
 vim.o.backupcopy = 'yes'
 
--- Fold second nesting
-vim.opt.foldlevel = 2
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- NOTE: Only fold 3rd level
+vim.cmd('set foldlevel=3')
+-- NOTE: Fold using treesitter
+vim.cmd('set foldmethod=expr')
+vim.cmd('set foldexpr="nvim_treesitter#foldexpr()"')
+-- NOTE: Start with no fold
+vim.cmd('set foldlevelstart=99')
 
--- This was set for jdtls i think
+-- WARNING: This was set for jdtls
 vim.cmd('filetype plugin on')
 
--- Really annoying having this as <c-c>
+-- NOTE: Overriding default nvim sql support
 vim.cmd("let g:ftplugin_sql_omni_key = '<C-p>'")
 
--- Colorscheme
+-- NOTE: Colorscheme
 vim.g.catppuccin_flavour = 'mocha'
 vim.api.nvim_command("colorscheme catppuccin")
 
--- Vimtex pdf viewer
+-- NOTE: Vimtex pdf viewer
 vim.g.vimtex_view_method = "sioyek"
+vim.g.vimtex_compiler_latexmk = {
+	out_dir = 'out'
+}
 
-vim.g.indentLine_fileTypeExclude = {'dashboard', 'startify'}
-
-vim.g.foldlevelstart = 99
+-- NOTE: Codeium settings
+vim.g.codeium_disable_bindings = 1
+vim.g.codeium_render = false
