0b133dbaba9f48d50c6c9f66c77c2b99a2c913c2
mikaeleythor <eythor108@gmail.com>
2023-07-18 20:40:03 +0000

    Refactor plugin-specific options to plugin files

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,72 +1,36 @@
---vim.bo.autoindent = true
+-- NOTE: This was set for a livepreview
+vim.o.backupcopy = 'yes'
+vim.g.syntax_on = true
+
+-- NOTE: Override this in ftplugin/.. if necessary
 vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
-
-vim.opt.termguicolors = true
-
--- NOTE: Linewidth
+vim.bo.expandtab = false
 vim.wo.cc = "80"
-
--- NOTE: Highlight current line
 vim.wo.cursorline = true
-
--- NOTE: Highlight search results
+vim.wo.relativenumber = true
+vim.wo.number = true
 vim.go.hlsearch = true
-
--- NOTE: Ignore case when searching
 vim.go.ignorecase = true
-
--- NOTE: Search incrementally (traverse between results with n/N)
 vim.go.incsearch = true
-
--- NOTE: Split screen buffers
 vim.go.splitright = true
 vim.go.splitbelow = true
-
--- NOTE: Count relative to current line
-vim.wo.relativenumber = true
-
--- NOTE: Expand tabs to spaces
-vim.bo.expandtab = true
-
--- NOTE: This sets the clipboard to global
 vim.go.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
+
+-- NOTE: Taken from catppuccin docs
+vim.g.catppuccin_flavour = 'mocha'
+vim.api.nvim_command("colorscheme catppuccin")
 
 -- WARNING: Testing this for image.nvim
 vim.opt.conceallevel = 2
 vim.opt.signcolumn = "yes:2"
 
--- NOTE: Show number of current line
-vim.wo.number = true
-vim.g.syntax_on = true
-
--- WARNING: Set for 'livepreview'
-vim.o.backupcopy = 'yes'
-
--- NOTE: Only fold 3rd level
+-- WARNING: Works now don't touch!
 vim.cmd('set foldlevel=3')
--- NOTE: Fold using treesitter
 vim.cmd('set foldmethod=expr')
 vim.cmd('set foldexpr="nvim_treesitter#foldexpr()"')
--- NOTE: Start with no fold
 vim.cmd('set foldlevelstart=99')
 
--- WARNING: This was set for jdtls
-vim.cmd('filetype plugin on')
-
--- NOTE: Overriding default nvim sql support
-vim.cmd("let g:ftplugin_sql_omni_key = '<C-p>'")
-
--- NOTE: Colorscheme
-vim.g.catppuccin_flavour = 'mocha'
-vim.api.nvim_command("colorscheme catppuccin")
-
--- NOTE: Vimtex pdf viewer
-vim.g.vimtex_view_method = "sioyek"
-vim.g.vimtex_compiler_latexmk = {
-	out_dir = 'out'
-}
-
--- NOTE: Codeium settings
-vim.g.codeium_disable_bindings = 1
-vim.g.codeium_render = false
+vim.cmd('filetype plugin on') -- NOTE: This was set for jdtls at some point
+vim.cmd("let g:ftplugin_sql_omni_key = '<C-p>'") -- NOTE: Better than <c-c>
---
+++ lua/user/vimtex.lua
@@ -0,0 +1,4 @@
+vim.g.vimtex_view_method = "sioyek"
+vim.g.vimtex_compiler_latexmk = {
+	out_dir = 'out'
+}
