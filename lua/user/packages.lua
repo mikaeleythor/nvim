30fe4c6e5ea5fa0607326d60418467a0038299b8
mikaeleythor <eythor108@gmail.com>
2023-07-26 09:46:26 +0000

    Refactor neorg configs to separate module

--- lua/user/packages.lua
+++ lua/user/packages.lua
@@ -130,48 +130,7 @@ return {
 		"nvim-neorg/neorg",
 		commit = '67c729f',
 		config = function()
-			require('neorg').setup({
-				load = {
-					["core.defaults"] = {}, -- Loads default behaviour
-					["core.concealer"] = {}, -- Adds pretty icons to your documents
-					["core.summary"] = {},
-					["core.dirman"] = { -- Manages Neorg workspaces
-						config = {
-							workspaces = {
-								school = "~/Documents/notes/school",
-								linux = "~/Documents/notes/linux",
-								work = "~/Documents/notes/work",
-							},
-							default_workspace = "linux",
-						},
-					},
-					["core.integrations.telescope"] = {},
-					--["external.hello-world"] = {},
-					["external.mermaid"] = {},
-					["core.looking-glass"] = {},
-					-- HACK: Localleader keybinds
-					["core.keybinds"] = {
-						config = {
-							hook = function(keybinds)
-								-- Binds the `gtd` key in `norg` mode to execute `:echo 'Hello'`
-								keybinds.map("norg", "n", "<localleader>fi",
-									"<cmd>Neorg index<CR>")
-								keybinds.map("norg", "n", "<localleader>fh",
-									"<cmd>Telescope neorg search_headings<CR>")
-								keybinds.map("norg", "n", "<localleader>jd",
-									"<cmd>Neorg journal today<CR>")
-								keybinds.map("norg", "n", "<localleader>jm",
-									"<cmd>Neorg journal tomorrow<CR>")
-								keybinds.map("norg", "n", "<localleader>jt",
-									"<cmd>Neorg journal toc<CR>")
-								keybinds.map("norg", "n", "<localleader>fl",
-									"<cmd>Neorg keybind all core.looking-glass.magnify-code-block<CR>")
-							end,
-						}
-					}
-				},
-				dependencies = { dir = "~/Desktop/lua/mermaid/module.lua" },
-			})
+			require('neorg').setup(require('user.neorg'))
 		end,
 		run = ":Neorg sync-parsers",
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-neorg/neorg-telescope" },
