62dd09342c9e6f851e669e2f591a6c8cf7c68b0d
mikaeleythor <eythor108@gmail.com>
2023-12-18 17:42:50 +0000

    Format and cleanup

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -1,20 +1,41 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
+vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
+
+local border = {
+	{ "┌", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "┐", "FloatBorder" },
+	{ "│", "FloatBorder" },
+	{ "┘", "FloatBorder" },
+	{ "─", "FloatBorder" },
+	{ "└", "FloatBorder" },
+	{ "│", "FloatBorder" },
+}
+
+-- Add the border on hover and on signature help popup window
+local handlers = {
+	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+}
 
 -- NOTE: Neodev must be set up before lspconfig
 require("neodev").setup({})
-local lspconfig = require('lspconfig')
-lspconfig['lua_ls'].setup {
+local lspconfig = require("lspconfig")
+lspconfig["lua_ls"].setup({
+	handlers = handlers,
 	capabilities = capabilities,
 	settings = {
 		Lua = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = 'LuaJIT',
+				version = "LuaJIT",
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
-				globals = { 'vim', 'use' }
+				globals = { "vim", "use" },
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
@@ -26,52 +47,54 @@ lspconfig['lua_ls'].setup {
 			},
 		},
 	},
-}
-lspconfig['pylsp'].setup {
-	capabilities = capabilities
-}
-lspconfig['jsonls'].setup {
-	capabilities = capabilities
-}
-lspconfig['marksman'].setup {
-	capabilities = capabilities
-}
-lspconfig['bashls'].setup {
-	capabilities = capabilities
-}
-lspconfig['dockerls'].setup {
-	capabilities = capabilities
-}
-lspconfig['tsserver'].setup {
+})
+lspconfig["pylsp"].setup({
+	handlers = handlers,
 	capabilities = capabilities,
-}
---lspconfig['eslint'].setup {
---capabilities = capabilities,
---filetypes = { "javascript" }
---}
-lspconfig['texlab'].setup {
-	capabilities = capabilities
-}
-lspconfig['clangd'].setup {
-	capabilities = capabilities
-}
-lspconfig['cssls'].setup {
+})
+lspconfig["jsonls"].setup({
+	handlers = handlers,
+	capabilities = capabilities,
+})
+lspconfig["marksman"].setup({
+	handlers = handlers,
+	capabilities = capabilities,
+})
+
+lspconfig["bashls"].setup({
+	handlers = handlers,
+	capabilities = capabilities,
+})
+lspconfig["dockerls"].setup({
+	handlers = handlers,
+	capabilities = capabilities,
+})
+lspconfig["tsserver"].setup({
+	handlers = handlers,
+	capabilities = capabilities,
+})
+lspconfig["texlab"].setup({
+	handlers = handlers,
+	capabilities = capabilities,
+})
+lspconfig["clangd"].setup({
+	handlers = handlers,
+	capabilities = capabilities,
+})
+lspconfig["cssls"].setup({
+	handlers = handlers,
 	capabilities = capabilities,
 	indent = false,
-}
--- html lsp server is not needed since it
--- doesn't provide formatting or diagnostics
-
---lspconfig['html'].setup {
---capabilities = capabilities,
---}
-lspconfig['spectral'].setup {
-	capabilities = capabilities
-}
-lspconfig['emmet_ls'].setup {
+})
+lspconfig["spectral"].setup({
+	handlers = handlers,
+	capabilities = capabilities,
+})
+lspconfig["emmet_ls"].setup({
+	handlers = handlers,
 	capabilities = capabilities,
 	--capabilities.textDocument.completion.completionItem.snippetSupport = true
-	filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
 	init_options = {
 		html = {
 			options = {
@@ -79,36 +102,42 @@ lspconfig['emmet_ls'].setup {
 				["bem.enabled"] = true,
 			},
 		},
-	}
-}
---lspconfig['sqls'].setup {
-	--on_attach = function(client, bufnr)
-		--require('sqls').on_attach(client, bufnr)
-	--end,
-	----capabilities = capabilities,
---}
+	},
+})
 
 -- When the arduino server starts in these directories, use the provided FQBN.
 -- Note that the server needs to start exactly in these directories.
 -- This example would require some extra modification to support applying the FQBN on subdirectories!
 local my_arduino_fqbn = {
-    ["/home/cinder/engx/3week/data-transmission"] = "arduino:avr:feather32u4",
+	["/home/cinder/engx/3week/data-transmission"] = "arduino:avr:feather32u4",
 }
 
 local DEFAULT_FQBN = "arduino:avr:feather32u4"
 
-lspconfig.arduino_language_server.setup {
-    on_new_config = function (config, root_dir)
-        local fqbn = my_arduino_fqbn[root_dir]
-        if not fqbn then
-            vim.notify(("Could not find which FQBN to use in %q. Defaulting to %q."):format(root_dir, DEFAULT_FQBN))
-            fqbn = DEFAULT_FQBN
-        end
-        config.cmd = {
-            "arduino-language-server",
-            "-cli-config", "/home/cinder/.arduino15/arduino-cli.yaml",
-            "-fqbn",
-            fqbn
-        }
-    end
-}
+lspconfig.arduino_language_server.setup({
+	handlers = handlers,
+	on_new_config = function(config, root_dir)
+		local fqbn = my_arduino_fqbn[root_dir]
+		if not fqbn then
+			vim.notify(("Could not find which FQBN to use in %q. Defaulting to %q."):format(root_dir, DEFAULT_FQBN))
+			fqbn = DEFAULT_FQBN
+		end
+		config.cmd = {
+			"arduino-language-server",
+			"-cli-config",
+			"/home/cinder/.arduino15/arduino-cli.yaml",
+			"-fqbn",
+			fqbn,
+		}
+	end,
+})
+
+lspconfig["vimls"].setup({
+	capabilities = capabilities,
+})
+
+lspconfig["ltex"].setup({})
+lspconfig["matlab_ls"].setup({
+	handlers = handlers,
+	capabilities = capabilities,
+})
