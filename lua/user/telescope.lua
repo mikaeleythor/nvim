c5ff9a9abca8de6ef1f449c94de1fc1619f58804
mikaeleythor <eythor108@gmail.com>
2023-12-18 17:49:32 +0000

    Add gpt plugin

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -2,8 +2,27 @@ local telescope = require("telescope")
 telescope.setup({
 	defaults = {
 		file_ignore_patterns = {
-			"node_modules"
-		}
+			"node_modules",
+		},
+	},
+	extensions = {
+		gpt = {
+			title = "Gpt Actions",
+			commands = {
+				"add_tests",
+				"chat",
+				"docstring",
+				"explain_code",
+				"fix_bugs",
+				"grammar_correction",
+				"interactive",
+				"optimize_code",
+				"summarize",
+				"translate",
+			},
+			theme = require("telescope.themes").get_dropdown({}),
+		},
 	},
 })
-telescope.load_extension('possession')
+telescope.load_extension("possession")
+telescope.load_extension("gpt")
